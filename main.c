1da19b40dbe3b967b84ee9d6c95bca66abb70d81
Gonxa6282 <70608072+Gonxa6282@users.noreply.github.com>
2020-09-02 05:17:05 -0300

    123 prueba pruebita hola holita
    Prueba pruebita hola holita

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n1, n2, n3;
+    printf("Inicio del programa MAS PIOLA que van a ver en sus vidas.\n");
+    printf("Ingresa el numero 1: \n");
+    scanf("%d", & n1);
+    printf("Ingresa el numero 2: \n");
+    scanf("%d", & n2);
+    printf("Ingresa el numero 3: \n");
+    scanf("%d", 6 n3);
+
+
+    printf("Toma, aca tenes el numero %d, %d, y %d. Alto programa, ¿no?\n", n1, n2, n3);
+    return 0;
+}
